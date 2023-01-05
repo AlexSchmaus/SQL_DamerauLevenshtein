ef4642a92971fe2787d9de8798510dfe60be4128
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2023-01-05 16:01:32 -0500

    Bad start, try again!

--- DamLev_Function.sql
+++ DamLev_Function.sql
@@ -7,24 +7,34 @@
 DECLARE @A varchar(256) = 'Alex Shmus'
 DECLARE @B varchar(256) = 'Alex Schmaus'
 
--- The size of the alphabet
-DECLARE @Alpha int = 26
 -- The output - how "different" are the strings?
 DECLARE @Distance int
 
+DECLARE @ALen int = len(@A)
+DECLARE @BLen int = len(@B)
+
+-- There needs to be something to work with!
+IF @A IS NULL OR @B IS NULL OR @ALen = 0 OR @BLen = 0
+	RETURN
+	   
+-- Create a numbers table 
+-- 5 passes get us to 256, which is the lentgh of our input variables
+DECLARE @Num table (N int)
+;WITH 
+	C1 AS (SELECT 1 AS N),
+	C2 AS (SELECT 1 AS N FROM C1 UNION ALL SELECT N FROM C1),
+	C3 AS (SELECT 1 AS N FROM C2 A CROSS APPLY C2 B),
+	C4 AS (SELECT 1 AS N FROM C3 A CROSS APPLY C3 B),
+	C5 AS (SELECT 1 AS N FROM C4 A CROSS APPLY C4 B)
+INSERT INTO @Num
+SELECT
+	ROW_NUMBER() OVER (ORDER BY N)
+FROM C5
 
 
-DECLARE @da table (i int, val int)
+DECLARE @Max int = @ALen + @BLen
 
-;WITH cteNums AS (
-	SELECT 
-		ROW_NUMBER() OVER (ORDER BY name) AS RwNm
-	FROM sys.objects
-)
-INSERT INTO @da
-SELECT 
-	RwNm, 0
-FROM cteNums
-WHERE RwNm <= @Alpha
+DECLARE @ATbl table (Letter varchar, Distance int)
+DECLARE @BTbl table (Letter varchar, Distance int)
 
-SELECT * FROM @da
+
